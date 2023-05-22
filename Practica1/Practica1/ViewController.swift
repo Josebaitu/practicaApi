a7b19a5874ac4939779f534e9463a244f3645ae9
Eneko Díaz Romero <eneko.diaz.romero@gmail.com>
2023-05-22 22:34:44 +0200

    fix buscarInformacion

--- Practica1/Practica1/ViewController.swift
+++ Practica1/Practica1/ViewController.swift
@@ -34,20 +34,34 @@ class ViewController: UIViewController {
     }
     
     func buscarInformacion() {
-        let urlString = "https://randomuser.me/api/"
-        if let url = URL(string: urlString){
-            if let dato = try? Data(contentsOf: url){
-                let decodificador = JSONDecoder()
-                
-                if let datosDecodificados = try? decodificador.decode(Informacion.self, from: dato){
-                    
-                    informacionMia = datosDecodificados.results
-                    miTabla.reloadData()
+        // usamos "!" porque sabemos que esta URL está bien formada
+        let url = URL(string: "https://randomuser.me/api/")!
+        let request = URLRequest(url: url)
+        // definimos la query y le decimos que hacer cuando se complete (todavía no estamos ejecutándola)
+        let dataTask = URLSession.shared.dataTask(with: request) { data, response, error in
+            // Este trozo de código se está ejecutando en un hilo secundario para dejar el hilo principal libre
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                guard let datosDecodificados = try? JSONDecoder().decode(Informacion.self, from: data) else {
+                    fatalError("algo estás haciendo mal en la decodificación :)")
+                }
+                self.informacionMia = datosDecodificados.results
+                // Refresca la interfaz de usuario en el hilo principal
+                DispatchQueue.main.async {
+                    // El hilo principal es un núcleo del procesador del móvil que se encarga de actualizar la pantalla,
+                    // recoger la interacción del usuario, y otras tareas que siempre tienen que responder al momento.
+                    // Si intentas hacer una operación compleja en el hilo principal (como una petición de red o una
+                    // operación matemática complicada), el hilo principal va a estar ocupado haciendo esa operación
+                    // y la pantalla se va a congelar. Apple nunca permite eso, y de hecho si intentas publicar una app
+                    // así te la rechazará. Es por eso que Xcode te avisa de que estás haciéndolo mal
+                    self.miTabla.reloadData()
                 }
             }
         }
+        // le decimos a la query que se ejecute
+        dataTask.resume()
     }
-
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
@@ -57,8 +71,9 @@ func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> 
 
 func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let celda = miTabla.dequeueReusableCell(withIdentifier: "celdaInfo", for: indexPath)
-    //celda.textLabel?.text = informacionMia[indexPath]
-    //celda.detailTextLabel?.text = informacionMia[indexPath]
+    let info = informacionMia[indexPath.row]
+    celda.textLabel?.text = info.email
+    celda.detailTextLabel?.text = info.gender
     
     return celda
 }
